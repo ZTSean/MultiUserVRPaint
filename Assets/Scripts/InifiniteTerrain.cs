b029349c479bc7c04653a2972487034cf99ed8ce
Zixiao Wang <Zixiao Wang>
2016-03-21 13:57:27 -0500

    first commit

---
+++ Assets/Scripts/InifiniteTerrain.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InifiniteTerrain : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/TerrainGrid.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TerrainGrid : MonoBehaviour
+{
+	public float cellSize = 1;
+	public int gridWidth = 10;
+	public int gridHeight = 10;
+	public float yOffset = 0.5f;
+	public Material cellMaterialValid;
+	public Material cellMaterialInvalid;
+
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+}
